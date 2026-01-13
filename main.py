4760150788328b87a03ab559787765a8f5dc1e53
Muhammad Sayid Qosimov <qosimovmuhammadsayyid920@gmail.com>
2026-01-13 21:47:17 +0500

    main.py faylga ozgartirish kiritildi

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ class Patient:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    def new_patient(self, value: str):
-        if value.isdigit():
-            self.age = value
+    def new_patient(self, name: str, age: str):
+        if age.isdigit():
+            self.age = int(age)
         else:
-            raise ValueError("yoshni sonda kiritish kerak")
-        if value[0].isupper():
-            self.name = value
+            raise ValueError("Yoshni sonda kiritish kerak")
+        if name[0].isupper():
+            self.name = name
         else:
             raise ValueError("Ismi bosh harifi katta bolishi kerak")
 class Doctor:
@@ -17,7 +17,6 @@ class Doctor:
         self.specialty = specialty
         self.patient_type = patient_type
         self.experience = experience
-
     def get_info_doctors(self):
         return f"Shifokor: {self.name}, Yonalishi: {self.specialty}, Kimlarni koradi: {self.patient_type}, Tajribasi: {self.experience}"
     
@@ -26,18 +25,15 @@ class Doctor:
             self.name = value
         else:
             raise ValueError("Ismning boshida 'Dr.' bolishi shart")
-        
 class Appointment:
     def __init__(self, data, patient, chosen_doctor):
         self.data = data
         self.patient = patient
         self.doctor = chosen_doctor
-            
     def show_appointment(self):
         print(f"Sana: {self.data}")
         print(f"Bemor: {self.patient.name}, {self.patient.age} yosh")
         print(f"Shifokor: {self.doctor.name}, {self.doctor.specialty}")
-
 
 doctor_ear = Doctor("Dr. Valijon", "Quloq salomatligi", "Hamma uchun", "8 yil")
 doctor_brain = Doctor("Dr. Alijon", "Miyya salomatligi", "Hamma uchun", "12 yil")
@@ -50,7 +46,7 @@ patients = []
 def add_patient(name, age):
     patient = Patient(name, age)
     patients.append(patient)
-    print(f"yangi bemor qoshildi: {patient.name}, {patient.age} yosh")
+    print(f"Yangi bemor qoshildi: {patient.name}, {patient.age} yosh")
     return patient
 patient1 = add_patient("Laylo", 8)
 patient2 = add_patient("Lobar", 11)
@@ -60,35 +56,34 @@ appointments = []
 def add_appointment(date, patient, doctor):
     appointment = Appointment(date, patient, doctor)
     appointments.append(appointment)
-    print(f"Yangi bemor ochret qoshildi: {patient.name}, shifokor ismi: {doctor.name}")
+    print(f"Yangi bemor ochret qoshildi: {patient.name}, Shifokor ismi: {doctor.name}")
     return appointment
 
+doctor_children.set_name("Dr. Rustamjon")
+print("Bolalar doktori yangi tayinlandi:", doctor_children.name)
+appointment1 = add_appointment("2025-05-23", patient1, doctor_children)
+appointment2 = add_appointment("2025-05-24", patient2, doctor_brain)
+appointment3 = add_appointment("2025-07-22", patient3, doctor_ear)
+appointment1.show_appointment()
+print()
+appointment2.show_appointment()
+print()
+appointment3.show_appointment()
+print()
 
-# doctor_children.set_name("Dr. Rustamjon")
-# print("Bolalar doktori yangi tayinlandi:", doctor_children.name)
-# appointment1 = add_appointment("2025-05-23", patient1, doctor_children)
-# appointment2 = add_appointment("2025-05-24", patient2, doctor_brain)
-# appointment3 = add_appointment("2025-07-22", patient3, doctor_ear)
-# appointment1.show_appointment()
-# print()
-# appointment2.show_appointment()
-# print()
-# appointment3.show_appointment()
-# print()
+print("Shifo xona doktorlar royhati:")
+for doc in doctors:
+    print(doc.get_info_doctors())
 
-# print("Shifo xona doktorlar royhati:")
-# for doc in doctors:
-#     print(doc.get_info_doctors())
-
-# def find_doctor_name(name, doctors):
-#     for doc in doctors:
-#         if doc.name == name:
-#             return doc
-#         return None
-# search_name = "Dr. Rustamjon"
-# doctor_found = find_doctor_name(search_name, doctors)
-# if doctor_found:
-#     print("Topilgan shifokor malumoti")
-#     print(doctor_found.get_info_doctors())
-# else:
-#     print(f"{search_name} ismli shifokor topilmadi")# Main program file
+def find_doctor_name(name, doctors):
+    for doc in doctors:
+        if doc.name == name:
+            return doc
+        return None
+search_name = "Dr. Rustamjon"
+doctor_found = find_doctor_name(search_name, doctors)
+if doctor_found:
+    print("Topilgan shifokor malumoti")
+    print(doctor_found.get_info_doctors())
+else:
+    print(f"{search_name} ismli shifokor topilmadi")
